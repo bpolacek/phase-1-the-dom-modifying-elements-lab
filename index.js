f8bef7a7c48f63f441b1b8890f75e36d0577be57
Brett Polacek <brettpolacek@gmail.com>
2023-02-04 17:11:33 -0500

    added index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+let element = document.querySelector("main#main");
+    element.remove();
+
+    const newHeader = document.createElement('h1');
+    console.log(newHeader);
+    newHeader.id = 'victory';
+    newHeader.innerHTML = `Brett is the champion`;
+
